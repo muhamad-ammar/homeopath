969b0d4393b83cff8bc9fce0ff4159ccbf26de77
muhamad-ammar <ammar.bscs18seecs2seecs.edu.pk>
2021-08-26 15:34:42 +0500

    Url problem fixed

--- module1/urls.py
+++ module1/urls.py
@@ -1,6 +1,8 @@
+from os import name
+from module1.views import login
 from django.contrib import admin
 from django.urls import path
-
+from .views  import login
 urlpatterns = [
-    path('login/', admin.site.urls),
+    path('login/',login,name="login" ),
 ]
