9cb1dbaca9217ea88c0f91a622bf0c6f340ba268
muhamad-ammar <ammar.bscs18seecs2seecs.edu.pk>
2021-08-26 16:01:11 +0500

    Register is implemented

--- module1/urls.py
+++ module1/urls.py
@@ -2,7 +2,8 @@ from os import name
 from module1.views import login
 from django.contrib import admin
 from django.urls import path
-from .views  import login
+from .views  import register,login
 urlpatterns = [
     path('login/',login,name="login" ),
+    path('register/',register,name="register" ),
 ]
