6f1b9a4adad805a54853b6f59d16dcadcf5f07f6
muhamad-ammar <ammar.bscs18seecs@seecs.edu.pk>
2021-09-05 15:25:39 +0500

    Added Navbar and Footer

--- fypmodule1/algo.py
+++
@@ -1,7 +0,0 @@
-import os
-from pymongo import MongoClient, collection
-client = MongoClient("mongodb+srv://ammar:ammar1122@kent.7ewag.mongodb.net/kent?retryWrites=true&w=majority")
-db = client.cluster["kent"]
-collection=db["jsondata"]
-
-obj=db.collection.find("{Nat-m.:green}")
