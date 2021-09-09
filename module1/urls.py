b787ac6cc293cc5434a86e9a219f6565aab51c5d
muhamad-ammar <ammar.bscs18seecs@seecs.edu.pk>
2021-09-09 16:55:13 +0500

    '' updated

--- module1/urls.py
+++ module1/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 from .views import login_view, logout_view,Home_View, register_view,search_view,table_view
 urlpatterns = [
 
-     path('login/', login_view,name="login"),
+    path('login/', login_view,name="login"),
+    path('', login_view,name="login"),
     path('logout/', logout_view,name="logout"),
     path('register/', register_view,name="register"),
     path('home', Home_View,name="home"),
